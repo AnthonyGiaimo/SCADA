51d6a0684ef37143d965dfb90a8cce25db3200e8
Tony Desktop <c20anthony.giaimo@usafa.edu>
2019-04-26 14:19:30 -0600

    Session loggin now for more security

--- Main.py
+++ Main.py
@@ -3,28 +3,20 @@ import time
 import requests
 import datetime
 
-# Setup
-arduino_data = serial.Serial('com3', 9600)
-
-currentTime = time.localtime()
-day, year, month, hour, min, sec = currentTime.tm_mday, currentTime.tm_year, currentTime.tm_mon, \
-                                   currentTime.tm_hour, currentTime.tm_min, currentTime.tm_sec
-end = False
-
-answer = None
 
 def sessionLoggin():
     username = 'r0lyp0ly'
     password = '300thsquaD*muso'
     remember_me = 'on'
     data = {
-        'username':username, 'password':password, 'remember_me':remember_me
+        'username': username, 'password': password, 'remember_me': remember_me
     }
     session = requests.session()
     session.post(url="https://tony-giaimo.us/login.php", data=data)
     return session
 
-def getTime(session):
+
+def getTime(session, year, month, day):
     # api-endpoint
     URL = "https://tony-giaimo.us/coffee.php"
 
@@ -43,27 +35,42 @@ def getTime(session):
     return response.text
 
 
-session = sessionLoggin()
+def main():
+    # Setup
+    arduino_data = serial.Serial('com3', 9600)
 
-while not end:
     currentTime = time.localtime()
-    day, year, month, hour, min, sec = str(currentTime.tm_mday), str(currentTime.tm_year), str(currentTime.tm_mon), \
-                                       str(currentTime.tm_hour), str(currentTime.tm_min), str(currentTime.tm_sec)
-    # with the port open, the response will be buffered
-    # so wait a bit longer for response here
-    # Serial read section
+    day, year, month, hour, minute, sec = currentTime.tm_mday, currentTime.tm_year, currentTime.tm_mon, \
+                                       currentTime.tm_hour, currentTime.tm_min, currentTime.tm_sec
+    end = False
 
-    msg = arduino_data.readline().strip(b'\n').decode()
-    print("Message from arduino: ")
-    print(msg)
-    if msg == "clockCheck":
-        arduino_data.write(bytes(hour, 'utf-8'))
-        print("wrote to arduino")
-    if msg == "alarmSet":
-        arduino_data.write(bytes(getTime(session), 'utf-8'))
-        print("wrote to arduino")
-    if msg == "exit":
-        end = True;
-    msg = ""
-# Close session after done running while loop
-session.close()
+    answer = None
+
+    session = sessionLoggin()
+
+    while not end:
+        currentTime = time.localtime()
+        day, year, month, hour, minute, sec = str(currentTime.tm_mday), str(currentTime.tm_year), \
+                                              str(currentTime.tm_mon), str(currentTime.tm_hour), \
+                                              str(currentTime.tm_min), str(currentTime.tm_sec)
+        # with the port open, the response will be buffered
+        # so wait a bit longer for response here
+        # Serial read section
+
+        msg = arduino_data.readline().strip(b'\n').decode()
+        print("Message from arduino: ")
+        print(msg)
+        if msg == "clockCheck":
+            arduino_data.write(bytes(hour, 'utf-8'))
+            print("wrote to arduino")
+        if msg == "alarmSet":
+            arduino_data.write(bytes(getTime(session, year, month, day), 'utf-8'))
+            print("wrote to arduino")
+        if msg == "exit":
+            end = True;
+        msg = ""
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
