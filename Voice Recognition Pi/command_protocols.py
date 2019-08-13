e74a4823155fae8f766119485214d9985527a83f
AnthonyGiaimo <36255203+AnthonyGiaimo@users.noreply.github.com>
2019-08-13 07:46:30 -0700

    Update command protocols to match need of raspberry pi

--- Voice Recognition Pi/command_protocols.py
+++ Voice Recognition Pi/command_protocols.py
@@ -8,21 +8,4 @@ def printTest():
     print("Hello World")
 
 
-def openProgram(program):
-    program = program.lower()
-    pathDict = Command_Library.load_dictionary("../Exodus/ResponseDictionary.txt")
-    if program == "calculator":
-        program = "calc"
-    programPath = find(program + ".exe", "C:\\")
-    if programPath != None:
-        os.startfile(programPath)
-    else:
-        print("No such program exists I'm sorry")
-
-def find(name, path):
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            return os.path.join(root, name)
-
-
 
